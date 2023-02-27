6ee5cfdda8fc19701584588c28b63581a085a0ff
kalkidan12 <kalget12@gmail.com>
2023-02-27 16:12:04 +0300

    client setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function App() {
-	return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+	return <div className="text-3xl font-bold underline">Hello world!</div>;
 }
